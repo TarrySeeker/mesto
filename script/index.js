e0eb1cb52b4fe6f4f7a09d645c5f3da7f2508631
Alexandr <akekbol@gmail.com>
2023-07-19 22:39:45 +0500

    revu

--- script/index.js
+++ script/index.js
@@ -2,16 +2,16 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 let popupElement = document.querySelector(".popup");
 let nameValue = document.querySelector(".profile__name");
-let jobValue = document.querySelector(".profile__description");
-let nameInput = document.querySelector('.popup__input_name_typed');
+let jobValue = document.querySelector(".profile__description"); //discript EditBut
+let nameInput = document.querySelector('.popup__input_data_name');
 let jobInput = document.querySelector('.popup__input_description_typed');
 const formElement = document.querySelector('.popup__form');
 
 
 function openedPopup() {
-  popupElement.classList.add('popup_opened');
   nameInput.value = nameValue.textContent;
   jobInput.value = jobValue.textContent;
+  popupElement.classList.add('popup_opened');
 }
 
 function closedPopup() {
